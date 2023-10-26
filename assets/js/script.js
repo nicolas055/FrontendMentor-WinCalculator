65611c52db8369d443ea857953edb437b08f387f
Nicolas Leite <nicolasleite7@gmail.com>
2023-10-26 09:45:56 -0300

    operation object done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 /* Change Theme */
 // Getting elements from html
 const body = document.querySelector('body');
@@ -33,13 +32,22 @@ toggle.addEventListener("click", changeTheme);
 // Getting elements from html
 const screen = document.getElementById('screen');
 const keypad = document.getElementById('keypad');
+const previousInput = document.getElementById('previous-input');
 
 // Regex
 const numbersAndSignsRgx = /[1234567890/*\-+.]/; // Only accepts numbers and signs
 const signsRgx = /[/*\-+]/; // accpets only signs
+const numbersRgx = /^[0-9]+$/ // accpets only numbers
+
 
 // Counters
 let typeCounter = 0;
+let signCounter = 0;
+let operation = {
+    n1: '',
+    sign: '',
+    n2: ''
+}
 
 // Functions
 // Give an empty value to screen
@@ -60,11 +68,25 @@ function deleteDefault0() {
     if (typeCounter <= 1) emptyScreen();
 }
 
-// Input numbers and signs
-function input(e) {
-    // Accpets the key input only if the key pressed is numbers or signs
-    if (numbersAndSignsRgx.test(e.key)) {
+// Input numbers
+function inputNumber(e) {
+    // Accpets the key input only if the key pressed is a number 
+    if (numbersRgx.test(e.key) && signCounter < 1) {
+        operation.n1 = operation.n1.concat(e.key);
         screen.append(e.key);
+    }
+    if (numbersRgx.test(e.key) && signCounter >= 1) {
+        operation.n2 = operation.n2.concat(e.key);
+    }
+}
+
+// Input signs
+function inputSign(e) {
+    // Accpets the key input only if the key pressed is a sign
+    if (signsRgx.test(e.key)) {
+        operation.sign = e.key;
+        signCounter += 1;
+        operation.n2 = '';
     }
 }
 
@@ -85,16 +107,20 @@ function reset(e) {
 // Show the result on screen
 function result(e) {
     if (e.key === 'Enter') {
-        screen.innerText = math.evaluate(screen.innerText);
+        screen.innerText = math.evaluate(operation.n1 + operation.sign + operation.n2);
+        operation.n1 = '' + math.evaluate(operation.n1 + operation.sign + operation.n2);
+        signCounter = 0;
     }
 }
 
 document.addEventListener('keydown', (e) => {
     typeCounter += 1;
     deleteDefault0();
-    input(e);
+    inputNumber(e);
+    inputSign(e)
     del(e);
     reset(e);
     result(e);
     default0();
+    console.log(operation)
 });
